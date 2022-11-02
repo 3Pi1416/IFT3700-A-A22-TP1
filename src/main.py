502e139f8947d7c0f18c1f1f80f38c10c8cf379d
Hugo <hugo.carrier@umontreal.com>
2022-11-02 04:54:19 -0400

    finishing mnist with doc

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 from keras.datasets import mnist
 
 
+#how to import mnist https://www.digitalocean.com/community/tutorials/mnist-dataset-in-python
 if __name__ == '__main__':
-    test = mnist
+    (data_to_train_X, data_to_train_y), (data_to_test_X, data_to_test_y) = mnist.load_data()
     print("wow")
