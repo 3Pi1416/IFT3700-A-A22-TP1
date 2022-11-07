80570608f77b005744b06dcf02705a1389cdcaca
Hugo <hugo.carrier@umontreal.com>
2022-11-07 08:10:47 -0500

    continue

--- src/KNeighbour.py
+++ src/KNeighbour.py
@@ -1,7 +1,9 @@
 from sklearn.neighbors import KNeighborsClassifier
 
 
-def evaluate_k_neighbour(data_set_x, data_set_y, test_set, dissimilarities, similarities):
+def evaluate_k_neighbour(data_set_y, data_set_dissimilarities, test_set_dissimilarities):
     knn = KNeighborsClassifier(n_neighbors=2, metric='precomputed', algorithm='brute')
-    knn.fit(dissimilarities, data_set_y)
-    wow = knn.predict(test_set)
+    knn.fit(data_set_dissimilarities, data_set_y)
+    wow = knn.predict(test_set_dissimilarities)
+
+    return wow
